4226eb0d80d9cfebd94a338fbd160c97a2fa78b6
tang <923233700@qq.com>
2020-09-15 08:41:06 +0800

    导航栏布局搞定

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,49 @@ import {
   Link,
   Redirect,
 } from "react-router-dom";
+import styled from "styled-components";
 
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+const Main = styled.div`
+  flex-grow: 1;
+`;
+const Nav = styled.nav`
+  border: 1px solid blue;
+  > ul {
+    display: flex;
+    > li {
+      width: 33.333%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`;
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
+      <Wrapper>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Redirect exact from="/" to="/money" />
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+        <Nav>
           <ul>
             <li>
               <Link to="/tags">标签页</Link>
@@ -24,24 +61,8 @@ function App() {
               <Link to="/statistics">统计页</Link>
             </li>
           </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/tags">
-            <Tags />
-          </Route>
-          <Route path="/money">
-            <Money />
-          </Route>
-          <Route path="/statistics">
-            <Statistics />
-          </Route>
-          <Redirect exact from="/" to="/money" />
-          <Route path="*">
-            <NoMatch />
-          </Route>
-        </Switch>
-      </div>
+        </Nav>
+      </Wrapper>
     </Router>
   );
 }
