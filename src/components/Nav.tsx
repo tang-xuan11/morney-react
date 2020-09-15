c4146f9657f57a7bc24155f967123a65fd195796
tang <923233700@qq.com>
2020-09-15 14:25:09 +0800

    修复 Nav 点击区域过小的问题

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Icon from "./Icon";
 
-
 const NavWrapper = styled.nav`
   box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   line-height: 24px;
@@ -12,14 +11,16 @@ const NavWrapper = styled.nav`
     > li {
       width: 33.333%;
       text-align: center;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      padding: 4px 0;
-      > .icon {
-        width: 24px;
-        height: 24px;
+      > a {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 4px 0;
+        > .icon {
+          width: 24px;
+          height: 24px;
+        }
       }
     }
   }
@@ -30,16 +31,22 @@ const Nav = () => {
     <NavWrapper>
       <ul>
         <li>
-          <Icon name="tag" />
-          <Link to="/tags">标签页</Link>
+          <Link to="/tags">
+            <Icon name="tag" />
+            标签页
+          </Link>
         </li>
         <li>
-          <Icon name="money" />
-          <Link to="/money">记账页</Link>
+          <Link to="/money">
+            <Icon name="money" />
+            记账页
+          </Link>
         </li>
         <li>
-          <Icon name="chart" />
-          <Link to="/statistics">统计页</Link>
+          <Link to="/statistics">
+            <Icon name="chart" />
+            统计页
+          </Link>
         </li>
       </ul>
     </NavWrapper>
