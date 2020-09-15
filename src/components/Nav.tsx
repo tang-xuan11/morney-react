a0e5954c71bf87aeabbe523bd6d9017552ae287a
tang <923233700@qq.com>
2020-09-15 09:13:37 +0800

    添加 Nav 样式，提取 Nav 组件

---
+++ src/components/Nav.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const NavWrapper = styled.nav`
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  line-height: 24px;
+  > ul {
+    display: flex;
+    > li {
+      width: 33.333%;
+      text-align: center;
+      padding: 16px;
+    }
+  }
+`;
+
+const Nav = () => {
+  return (
+    <NavWrapper>
+      <ul>
+        <li>
+          <Link to="/tags">标签页</Link>
+        </li>
+        <li>
+          <Link to="/money">记账页</Link>
+        </li>
+        <li>
+          <Link to="/statistics">统计页</Link>
+        </li>
+      </ul>
+    </NavWrapper>
+  );
+};
+
+export default Nav;
