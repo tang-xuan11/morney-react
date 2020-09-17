182dd202d89e32ffa919760b065c8247fedde172
tang <923233700@qq.com>
2020-09-17 11:18:42 +0800

    收集表单数据

--- src/views/Money/CategorySection.tsx
+++ src/views/Money/CategorySection.tsx
@@ -23,8 +23,14 @@ const Wrapper = styled.section`
     }
   }
 `;
-const CategorySection: React.FC = () => {
-  const [category, setCategory] = useState("-");
+
+type Props = {
+  value: "-" | "+";
+  onChange: (value: "-" | "+") => void;
+};
+
+const CategorySection: React.FC<Props> = (props) => {
+  const category = props.value;
   const [categoryList] = useState<("-" | "+")[]>(["-", "+"]);
   const categoryMap = { "-": "支出", "+": "收入" };
   return (
@@ -34,7 +40,7 @@ const CategorySection: React.FC = () => {
           <li
             className={category === c ? "selected" : ""}
             onClick={() => {
-              setCategory(c);
+              props.onChange(c);
             }}
             key={c}
           >
--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -32,9 +32,14 @@ const Wrapper = styled.section`
   }
 `;
 
-const TagsSection: React.FC = (props) => {
+type Props = {
+  value: string[];
+  onChange: (value: string[]) => void;
+};
+
+const TagsSection: React.FC<Props> = (props) => {
   const [tags, setTags] = useState<string[]>(["衣", "食", "住", "行"]);
-  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const selectedTags = props.value;
   const onAddTag = () => {
     const tagName = window.prompt("请输入标签名");
     if (tagName !== null) {
@@ -48,13 +53,13 @@ const TagsSection: React.FC = (props) => {
   const onToggleTag = (tag: string) => {
     const index = selectedTags.indexOf(tag);
     if (index >= 0) {
-      setSelectedTags(selectedTags.filter((t) => t !== tag));
+      props.onChange(selectedTags.filter((t) => t !== tag));
     } else {
-      setSelectedTags([...selectedTags, tag]);
+      props.onChange([...selectedTags, tag]);
     }
   };
   const getClass = (tag: string) => {
-   return selectedTags.indexOf(tag) >= 0 ? "selected" : "";
+    return selectedTags.indexOf(tag) >= 0 ? "selected" : "";
   };
   return (
     <Wrapper>
