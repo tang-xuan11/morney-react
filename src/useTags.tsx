6c6d44cc0a8a2bb7bdf83868dcb61d62215b027b
tang <923233700@qq.com>
2020-09-18 12:56:16 +0800

    修复 useTags 初始值的 bug

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -1,13 +1,14 @@
 import { createId } from "lib/createId";
 import { useState } from "react";
 
+const defaultTags = [
+  { id: createId(), name: "衣" },
+  { id: createId(), name: "食" },
+  { id: createId(), name: "住" },
+  { id: createId(), name: "行" },
+];
 const useTags = () => {
-  const [tags, setTags] = useState<{ id: number; name: string }[]>([
-    { id: createId(), name: "衣" },
-    { id: createId(), name: "食" },
-    { id: createId(), name: "住" },
-    { id: createId(), name: "行" },
-  ]);
+  const [tags, setTags] = useState<{ id: number; name: string }[]>(defaultTags);
   return { tags, setTags };
 };
 
